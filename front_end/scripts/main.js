153fdb8fa02eb304fbb3e04d9ec2e11909da8e2e
chiri <shenyichen0103@outlook.com>
2024-08-11 16:19:48 +0800

    loading animation

--- front_end/scripts/main.js
+++ front_end/scripts/main.js
@@ -1,3 +1,4 @@
+// Scroll to top
 $(function () {
   $(window).scroll(function () {
     var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度
@@ -15,3 +16,29 @@ $(function () {
 // Lazy load
 const observer = lozad();
 observer.observe();
+
+// Loadings
+$(window).on("load", function () {
+  disableScroll();
+  setTimeout(function () {
+    $(".loader-container").fadeOut("slow", function () {
+      enableScroll();
+    });
+  }, 3000);
+});
+
+function disableScroll() {
+  window.addEventListener("wheel", preventDefault, { passive: false });
+  window.addEventListener("DOMMouseScroll", preventDefault, { passive: false });
+  window.addEventListener("touchmove", preventDefault, { passive: false });
+}
+
+function enableScroll() {
+  window.removeEventListener("wheel", preventDefault);
+  window.removeEventListener("DOMMouseScroll", preventDefault);
+  window.removeEventListener("touchmove", preventDefault);
+}
+
+function preventDefault(e) {
+  e.preventDefault();
+}
